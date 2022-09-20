db8d6f48abf863671dddf196688ffaab098019e1
Mario Joao <mario.joao31@hotmail.com>
2022-09-20 18:27:35 +0100

    not deployed

--- myapp.py
+++ myapp.py
@@ -10,8 +10,10 @@ import socket
 sw = 1
 
 app = Flask(__name__)
-lista=["Desligado","Ligado"]
+lista=[]
 datelist=[]
+
+
 
 def get_ip_address():
         """ get ip-address of interface being used """
@@ -25,16 +27,16 @@ IP = get_ip_address()
 @app.route("/")
 @app.route("/home", methods =["POST", "GET"])
 def home():
-    #zipped=(lista,datelist)
+    zipped=(lista,datelist)
     if sw > 0:
-         return render_template("button.html", msg="Desligada",veri=False ,lista=lista, datelist=datelist)
+         return render_template("button.html", msg="Desligada",veri=False ,zipped=zipped)
     else:
-         return render_template("button.html", msg="Ligada", veri=True, lista=lista, datelist=datelist)
+         return render_template("button.html", msg="Ligada", veri=True, zipped=zipped)
     return render_template("button.html", lista=lista)   
 
 @app.route("/switch", methods =["POST", "GET"])
 def switch():
-    
+    zipped=(lista,datelist)
     global sw 
     if request.method == "POST":
         sw = sw * -1
@@ -43,18 +45,18 @@ def switch():
             dt=datetime.datetime.now()
             datelist.insert(0,dt)
             lista.insert(0,"Desligada")
-            print(lista)
-            return redirect(url_for("home",msg="Desligada" ,lista=lista), datelist=datelist) 
+            print(zipped)
+            return redirect(url_for("home",msg="Desligada" ,zipped=zipped)) 
         else:
             off()
             dt=datetime.datetime.now()
             datelist.insert(0,dt)
             lista.insert(0,"Ligada")
-            print(lista)
-            return redirect(url_for("home",msg="Ligada" ,lista=lista), datelist=datelist)
+            print(zipped)
+            return redirect(url_for("home",msg="Ligada" ,zipped=zipped))
 
     return render_template("button.html")
 
 if __name__ == "__main__":
-    app.run(host=IP, port=5000, debug=False)
+    app.run(threaded=True,host="192.168.1.102", port=5000, debug=False)
 
